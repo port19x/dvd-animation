f55440003d6fe739df3ee0607912a53e7fa264f0
port19 <port19@port19.xyz>
2022-12-15 07:35:44 +0100

    project setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject dvd-animation "0.1"
+  :description "a dvd animation written in clojure with quil"
+  :url "https://github.com/port19x/dvd-animation"
+  :license {:name "CC0"
+            :url "https://creativecommons.org/publicdomain/zero/1.0/legalcode"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [quil "3.1.0"]])
