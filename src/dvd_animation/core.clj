6bd2f8c9f92140e432f6e04b7136920e10d55778
port19 <port19@port19.xyz>
2022-12-16 22:39:13 +0100

    scaling

--- src/dvd_animation/core.clj
+++ src/dvd_animation/core.clj
@@ -8,22 +8,22 @@
   {:color (rand-int 256)
    :x 0
    :y 0
-   :vx 4
-   :vy 3})
+   :vx (/ (q/width) 125)
+   :vy (/ (* (q/height) 3) 500)})
 
 (defn update-state [state]
   ; Update sketch state by changing circle color and position.
-  {:color (if (or (>= (+ (:x state) (:vx state)) 400)
+  {:color (if (or (>= (+ (:x state) (:vx state)) (* (q/width) 0.8))
                   (<= (+ (:x state) (:vx state)) 0)
-                  (>= (+ (:y state) (:vy state)) 400)
+                  (>= (+ (:y state) (:vy state)) (* (q/height) 0.8))
                   (<= (+ (:y state) (:vy state)) 0)) (rand-int 256)
                                                      (:color state))
    :x (+ (:x state) (:vx state))
    :y (+ (:y state) (:vy state))
-   :vx (cond (>= (+ (:x state) (:vx state)) 400)  (* -1 (:vx state))
+   :vx (cond (>= (+ (:x state) (:vx state)) (* (q/width) 0.8))  (* -1 (:vx state))
              (<= (+ (:x state) (:vx state)) 0)    (* -1 (:vx state))
              :else                                (:vx state))
-   :vy (cond (>= (+ (:y state) (:vy state)) 400)  (* -1 (:vy state))
+   :vy (cond (>= (+ (:y state) (:vy state)) (* (q/height) 0.8))  (* -1 (:vy state))
              (<= (+ (:y state) (:vy state)) 0)    (* -1 (:vy state))
              :else                                (:vy state))
    })
@@ -33,7 +33,7 @@
   (q/background 0)
   ; Set rectangle color.
   (q/fill (:color state) 255 255)
-  (q/rect (:x state) (:y state) 100 100))
+  (q/rect (:x state) (:y state) (* (q/width) 0.2) (* (q/height) 0.2)))
 
 (q/defsketch dvd-animation
   :title "You spin my circle right round"
