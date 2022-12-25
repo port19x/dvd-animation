e694368da67c2aa21f2d8faa8a21f3d912d412ce
port19 <port19@port19.xyz>
2022-12-25 21:45:57 +0100

    cljs conversion

--- project.clj
+++ project.clj
@@ -4,4 +4,30 @@
   :license {:name "CC0"
             :url "https://creativecommons.org/publicdomain/zero/1.0/legalcode"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quil "3.1.0"]])
+                 [quil "3.1.0"]
+                 [org.clojure/clojurescript "1.10.520"]]
+
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-figwheel "0.5.19"]]
+  :hooks [leiningen.cljsbuild]
+
+  :clean-targets ^{:protect false} ["resources/public/js"]
+  :cljsbuild
+  {:builds [; development build with figwheel hot swap
+            {:id "development"
+             :source-paths ["src"]
+             :figwheel true
+             :compiler
+             {:main "dvd_animation.core"
+              :output-to "resources/public/js/main.js"
+              :output-dir "resources/public/js/development"
+              :asset-path "js/development"}}
+            ; minified and bundled build for deployment
+            {:id "optimized"
+             :source-paths ["src"]
+             :compiler
+             {:main "dvd_animation.core"
+              :output-to "resources/public/js/main.js"
+              :output-dir "resources/public/js/optimized"
+              :asset-path "js/optimized"
+              :optimizations :advanced}}]})
